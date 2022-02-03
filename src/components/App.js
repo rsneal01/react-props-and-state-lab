768791f4512bd30deec7e30b9e25b96cb14cebb1
rsneal01 <rsneal01@gmail.com>
2022-02-03 08:16:46 -0800

    commit tree 695db65ae877899cb2fa1982a0ed83cfadd894c7

--- src/components/App.js
+++ src/components/App.js
@@ -15,6 +15,28 @@ class App extends React.Component {
     }
   }
 
+  handleChangeType = ({target: {value}}) => {
+    this.setState({filters: {...this.state.filters, type: value}});
+  };
+
+  handleFindPets = () => {
+    let endpoint = '/api/pets';
+    if (this.state.filters.type !=='all') {
+      endpoint += `?type=${this.state.filters.type}`;
+    }
+
+    fetch(endpoint) 
+      .then(response => response.json())
+      .then(pets => this.setState({pets: pets}));
+  };
+
+  handleAdopt = (petId) => {
+    const pets = this.state.pets.map(pet => {
+      return pet.id === petId ? {...pet, isAdopted: true} : pet;
+    });
+    this.setState({pets: pets});
+  };
+  
   render() {
     return (
       <div className="ui container">
@@ -24,10 +46,10 @@ class App extends React.Component {
         <div className="ui container">
           <div className="ui grid">
             <div className="four wide column">
-              <Filters />
+              <Filters onChangeType={this.handleChangeType} onFindPetsClick={this.handleFindPets}/>
             </div>
             <div className="twelve wide column">
-              <PetBrowser />
+              <PetBrowser onAdoptPet={this.handleAdopt} pets={this.state.pets}/>
             </div>
           </div>
         </div>
